b13096112d2725a508e3d6051022e6aa8566423e
Madhan Mohan S <madhanmohans@Madhans-MacBook-Pro.local>
2024-09-10 17:23:56 +0530

    Add. styling

--- src/components/TimerProgressBar.js
+++ src/components/TimerProgressBar.js
@@ -6,14 +6,11 @@ import { styled } from "@mui/material/styles";
 
 const BorderLinearProgress = styled(LinearProgress)(() => ({
   height: 40,
-  borderRadius: 5,
-  width: "200px",
-  alignItems: "center",
+  width: "200px",    
   [`&.${linearProgressClasses.colorPrimary}`]: {
     backgroundColor: "#94a3b8",
   },
   [`& .${linearProgressClasses.bar}`]: {
-    borderRadius: 5,
     backgroundColor: "#0c4a6e"
   },
 }));
